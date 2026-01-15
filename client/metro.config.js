c8684a132ac9dd1d2455951672e0d015756e1240
farhandotme <princeofficial355@gmail.com>
2026-01-15 15:57:03 +0530

    done with nativewind small issues

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,8 +1,8 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-// Add CSS support
-config.resolver.assetExts.push('css');
-
-module.exports = config;
+module.exports = withNativeWind(config, {
+  input: "./global.css",
+});
