be19157ad1f712f4cc1710827537eeac7ccac32c
farhandotme <princeofficial355@gmail.com>
2026-01-16 23:55:04 +0530

    done with slides data and onboarding flatlist

--- client/app/onboarding/onboarding2.tsx
+++ client/app/onboarding/onboarding2.tsx
@@ -1,11 +1,44 @@
-import { View, Text } from "react-native";
+import { Dimensions, FlatList, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const { height, width } = Dimensions.get("window");
+
+const slides = [
+  {
+    id: "1",
+    image: require("../../assets/images/onboarding.png"),
+    title: "Instant Emargency Access",
+    discription:
+      "Doctor Can Instantly access Life-Saving Medical Information by Scanning your QR",
+  },
+  {
+    id: "2",
+    image: require("../../assets/images/onboarding2.png"),
+    title: "Every Second Matters",
+    discription:
+      "Blood group ,allergies and conditions available when you can't speak",
+  },
+  {
+    id: "3",
+    image: require("../../assets/images/onboarding3.png"),
+    title: "Private and Secure",
+    discription:
+      "Your data is encrypted and visible only in Medical Emargencies ",
+  },
+];
 
 const onboarding2 = () => {
   return (
-    <View>
-      <Text>onboarding2</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <FlatList
+        horizontal
+        data={slides}
+        contentContainerStyle={{ height: height * 0.75 }}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => <Slide item={item} />}
+      />
+    </SafeAreaView>
   );
 };
 
